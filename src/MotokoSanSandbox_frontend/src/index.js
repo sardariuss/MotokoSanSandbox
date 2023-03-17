7f4966e8b96403fe88cdbffaf98d89de58b739db
sardariuss <sardariuss@gmail.com>
2023-03-17 09:16:48 -0400

    Initial commit.

---
+++ src/MotokoSanSandbox_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { MotokoSanSandbox_backend } from "../../declarations/MotokoSanSandbox_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await MotokoSanSandbox_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
